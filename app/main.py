97614326a07a60979ca4acf9de7ce6d704fbc27f
kirbudilov01 <budilov02ke@gmail.com>
2025-12-25 15:07:09 +0300

    Update main.py

--- app/main.py
+++ app/main.py
@@ -1,1 +1,18 @@
+import asyncio
+from aiogram import Bot, Dispatcher
+from aiogram.fsm.storage.memory import MemoryStorage
 
+from app.config import BOT_TOKEN
+from app.handlers import start, menu_and_flow
+
+async def main():
+    bot = Bot(BOT_TOKEN)
+    dp = Dispatcher(storage=MemoryStorage())
+
+    dp.include_router(start.router)
+    dp.include_router(menu_and_flow.router)
+
+    await dp.start_polling(bot)
+
+if __name__ == "__main__":
+    asyncio.run(main())
