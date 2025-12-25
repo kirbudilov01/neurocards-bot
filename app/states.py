41002cfd761bc4851a847211147fbb40a800f89b
kirbudilov01 <budilov02ke@gmail.com>
2025-12-25 15:04:51 +0300

    Update states.py

--- app/states.py
+++ app/states.py
@@ -1,1 +1,8 @@
+from aiogram.fsm.state import StatesGroup, State
 
+class GenFlow(StatesGroup):
+    waiting_photo = State()
+    waiting_product_info = State()
+    waiting_template = State()
+    waiting_wishes = State()
+    waiting_confirm = State()
