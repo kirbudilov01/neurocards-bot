62e205374bfab6a20d5deb973d36bbdb6d1138a6
kirbudilov01 <budilov02ke@gmail.com>
2025-12-25 15:50:51 +0300

    Update start.py

--- app/handlers/start.py
+++ app/handlers/start.py
@@ -1,14 +1,14 @@
 from aiogram import Router
+from aiogram.filters import CommandStart
 from aiogram.types import Message
+
 from app.keyboards import kb_continue
 from app import texts
 from app.db import get_or_create_user
 
 router = Router()
 
-@router.message()
+@router.message(CommandStart())
 async def start_handler(message: Message):
-    if message.text and message.text.startswith("/start"):
-        get_or_create_user(message.from_user.id, message.from_user.username)
-        # тут позже добавишь видео-инструкцию (send_video). Пока текст + кнопка.
-        await message.answer(texts.WELCOME, reply_markup=kb_continue())
+    get_or_create_user(message.from_user.id, message.from_user.username)
+    await message.answer(texts.WELCOME, reply_markup=kb_continue())
