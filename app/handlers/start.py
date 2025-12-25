e80df6b641b34bce6dab97d1bbd16680e67931d5
kirbudilov01 <budilov02ke@gmail.com>
2025-12-25 18:43:49 +0300

    Update start.py

--- app/handlers/start.py
+++ app/handlers/start.py
@@ -1,6 +1,6 @@
 from aiogram import Router
-from aiogram.filters import CommandStart
 from aiogram.types import Message
+from aiogram.filters import CommandStart
 
 from app.keyboards import kb_continue
 from app import texts
@@ -10,5 +10,9 @@ router = Router()
 
 @router.message(CommandStart())
 async def start_handler(message: Message):
-    get_or_create_user(message.from_user.id, message.from_user.username)
-    await message.answer(texts.WELCOME, reply_markup=kb_continue())
+    try:
+        get_or_create_user(message.from_user.id, message.from_user.username)
+        await message.answer(texts.WELCOME, reply_markup=kb_continue())
+    except Exception as e:
+        await message.answer(f"❌ Ошибка на /start: {e}")
+        raise
